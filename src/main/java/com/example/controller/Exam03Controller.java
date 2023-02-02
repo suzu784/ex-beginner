5a7a00ec2597c71939372d5d64d90c7d954cdbf7
suzu784 <s.suzuki784@hotmail.com>
2023-02-02 16:10:39 +0900

    結果画面の作成

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.form.Ex03ReceiveForm;
@@ -21,18 +22,22 @@ public class Exam03Controller {
 		return "exam03";
 	}
 	
-	@GetMapping("")
-	public String count(Ex03ReceiveForm receiveForm) {
+	@PostMapping("/item-total")
+	public String itemTotal(Ex03ReceiveForm receiveForm) {
 		
 		int item1 = receiveForm.getIntItem1();
 		int item2 = receiveForm.getIntItem2();
 		int item3 = receiveForm.getIntItem3();
 		int total = item1 + item2 + item3; 
+		Double taxtotal = (item1 + item2 + item3) * 1.1; 
+		
 		
 		application.setAttribute("item1", item1);
 		application.setAttribute("item2", item2);
 		application.setAttribute("item3", item3);
 		application.setAttribute("total", total);
+		application.setAttribute("taxtotal", taxtotal);
+		
 		
 		return "exam03-result";
 	}
